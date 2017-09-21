63566c6548dc2aea30e3be94c7efb17065e97b71
laurent <laurent@fireworksprods.fr>
2017-09-21 14:35:30 +0200

    Working nav and recipes selection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
         new Recipe('A test', 'Another test', 'https://cdn.pixabay.com/photo/2017/04/04/01/08/fruit-2200001_960_720.jpg'),
         new Recipe('A second test', 'Another second test', 'http://knowledgeoverflow.com/wp-content/uploads/2013/03/food_photography_burger_by_masterdev777-d3h1ryk.jpg')
-
     ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
